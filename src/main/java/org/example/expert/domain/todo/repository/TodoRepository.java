1ec5c99f966a9ff09cae81127e98b6c5a288e92f
MJK-One <hanelejss012@naver.com>
2025-05-13 16:58:24 +0900

    Level 2 필수

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,30 +7,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepository {
 
-    @Query("""
-        SELECT t
-        FROM Todo t
-        LEFT JOIN FETCH t.user u
-        WHERE (:weather IS NULL OR t.weather = :weather)
-          AND (:start IS NULL OR t.modifiedAt >= :start)
-          AND (:end IS NULL OR t.modifiedAt <= :end)
-        ORDER BY t.modifiedAt DESC
-    """)
+
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user " +
+            "WHERE (:weather IS NULL OR t.weather LIKE %:weather%)" +
+            "AND (:startDay IS NULL OR DATE_FORMAT(t.modifiedAt, '%Y-%m-%d') >= :startDay)" +
+            "AND (:endDay IS NULL OR DATE_FORMAT(t.modifiedAt, '%Y-%m-%d') <= :endDay)" +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(
             @Param("weather") String weather,
             @Param("start") LocalDateTime start,
             @Param("end") LocalDateTime end,
             Pageable pageable
     );
-
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
